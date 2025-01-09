84cd459f5a3e3a9830f4a96f387f906f4558be10
Sniezka <dygons14@gmail.com>
2025-01-09 09:37:47 +0100

    Bump indexes

--- backend/src/plugins/db.plugin.ts
+++ backend/src/plugins/db.plugin.ts
@@ -8,21 +8,11 @@ export const connectDb = fastifyPlugin(async (app: FastifyInstance) => {
   const client = new MongoClient(DATABASE_URL);
   await client.connect();
   const db = client.db(DATABASE_NAME);
-  // TODO: Configure indexes
   // if index already exists this is just ignored
   const indexes = [
-    db.createIndex(
-      Collections.Referrals,
-      {
-        address: 1,
-        code: 1,
-        codeUsed: 1,
-        signature: 1,
-        message: 1,
-        invited: 1,
-      },
-      { unique: true }
-    ),
+    db.createIndex(Collections.Referrals, { address: 1 }, { unique: true }),
+    db.createIndex(Collections.Referrals, { code: 1, codeUsed: 1 }),
+    db.createIndex(Collections.Referrals, { invited: 1 }),
   ];
   await Promise.all(indexes);
   app.decorate("db", db);
